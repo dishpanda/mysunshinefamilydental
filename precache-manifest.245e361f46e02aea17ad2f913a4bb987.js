991d691fa75ceb4343b45d306e5d24f1c342f35e
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-07-12 20:39:16 -0700

    Updates

---
+++ precache-manifest.245e361f46e02aea17ad2f913a4bb987.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0a2c26305a767d056c0c40fa22a09b",
+    "url": "/mysunshinefamilydental/index.html"
+  },
+  {
+    "revision": "d4ae9564a98bb2e0e2fb",
+    "url": "/mysunshinefamilydental/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "74d74f082fcb9900b0f3",
+    "url": "/mysunshinefamilydental/static/css/main.ec7d6b7a.chunk.css"
+  },
+  {
+    "revision": "d4ae9564a98bb2e0e2fb",
+    "url": "/mysunshinefamilydental/static/js/2.5c5a2d7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mysunshinefamilydental/static/js/2.5c5a2d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d74f082fcb9900b0f3",
+    "url": "/mysunshinefamilydental/static/js/main.294541ff.chunk.js"
+  },
+  {
+    "revision": "ec44fca262ec6e5eaa05",
+    "url": "/mysunshinefamilydental/static/js/runtime-main.edd27c42.js"
+  },
+  {
+    "revision": "e8e0b0ee6dee53a58c3b596758de36bf",
+    "url": "/mysunshinefamilydental/static/media/logo.e8e0b0ee.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.233c73e8eefc42e39ba6ccca60bb1b4b.js"
+  "/mysunshinefamilydental/precache-manifest.245e361f46e02aea17ad2f913a4bb987.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mysunshinefamilydental/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
