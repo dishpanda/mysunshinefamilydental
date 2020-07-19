e1313698338df38b1e44f1cc97fa3b1d489a12dd
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-07-19 00:11:14 -0700

    Updates

---
+++ precache-manifest.4dfd80597f1fcf28a65ba8053d4375fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0676bffb786c04d5c5aa7e12efdb878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "748622dfe41a966c0386",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f6ef0c19727900b72a61",
+    "url": "/static/css/main.c1a16847.chunk.css"
+  },
+  {
+    "revision": "748622dfe41a966c0386",
+    "url": "/static/js/2.2c3e4b9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2c3e4b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ef0c19727900b72a61",
+    "url": "/static/js/main.3d52a31a.chunk.js"
+  },
+  {
+    "revision": "49d851db55e26f6756e7",
+    "url": "/static/js/runtime-main.1329c337.js"
+  },
+  {
+    "revision": "e8e0b0ee6dee53a58c3b596758de36bf",
+    "url": "/static/media/logo.e8e0b0ee.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mysunshinefamilydental/precache-manifest.e3fd70f2462babe6026fd0ff6350ac8d.js"
+  "/precache-manifest.4dfd80597f1fcf28a65ba8053d4375fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mysunshinefamilydental/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
